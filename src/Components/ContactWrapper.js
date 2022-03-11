ffaba5f78c730bedfe113b914fc637e856296afc
kai123-del <bhattkailash786@gmail.com>
2022-03-11 16:23:12 +0530

    second commit

--- src/Components/ContactWrapper.js
+++ src/Components/ContactWrapper.js
@@ -52,9 +52,9 @@ const ContactWrapper=(props)=>{
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{props.Email}</td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{props.Mobile}</td>
                     <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                      <a href="#" className="text-indigo-600 hover:text-indigo-900" onClick={DeleteHandler}>
+                      <button className="text-indigo-600 hover:text-indigo-900" onClick={DeleteHandler}>
                         Delete
-                      </a>
+                      </button>
                     </td>
                 
                 
